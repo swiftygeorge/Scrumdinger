d97af8fae124606debedafa14a0c67cfd1d46bdc
George Mapaya <lags-twisted-0b@icloud.com>
2022-06-17 19:54:54 -0400

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by George Mapaya on 2022-06-17.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
